8f5947f511cb463d109e40f6a6b4dfad38a9cdeb
Темергалеев Никита <mr.wonderwol@mail.ru>
2024-12-03 23:05:08 +0700

    finish

--- lab5/app.py
+++ lab5/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = '123'
 
 # Flask-Login настройка
 login_manager = LoginManager(app)
